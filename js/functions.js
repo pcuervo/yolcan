ccf874da45bf301c44a0418ca0dd5004c3874fda
arisbeth <arisbeth.hg@gmail.com>
2016-01-02 22:46:07 -0600

    estilo checkbox-recetas

--- js/functions.js
+++ js/functions.js
@@ -45,3 +45,18 @@ function init_masonry(){
         });
     });
 }
+
+
+//
+var input = document.querySelectorAll("label.check input");
+if(input !== null) {
+  [].forEach.call(input, function(el) {
+    if(el.checked) {
+      el.parentNode.classList.add('c_on');
+    }
+    el.addEventListener("click", function(event) {
+      event.preventDefault();
+      el.parentNode.classList.toggle('c_on');
+    }, false);
+  });
+}
